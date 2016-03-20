5ff8a65d5949b526e9b598c4f6486bc949054011
Johannes <jogus@live.de>
2016-03-20 22:59:10 +0100

    bug

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ define(["require", "exports", "jquery"], function (require, exports, $) {
     function addTransform(container, transform) {
         var old = container.css("transform") || "none";
         container.css("transform", transform + (old == "none" ? "" : " " + old));
+        return container;
     }
     function createChildFace(width, height, container) {
         if (!width)
@@ -23,26 +24,41 @@ define(["require", "exports", "jquery"], function (require, exports, $) {
         }
         return face;
     }
+    var classFaceOuter = "faceOuter";
+    var classFaceOuterFront = "faceOuterFront";
+    var classFaceOuterBack = "faceOuterBack";
+    var classFaceBlock = "faceBlock";
+    function createBackFace(front) {
+        return front;
+        var back = createChildFace(undefined, undefined, front);
+        back.addClass(classFaceOuter).addClass(classFaceOuterBack);
+        var block = createChildFace(undefined, undefined, front);
+        block.addClass(classFaceBlock);
+        return back;
+    }
     function pullOut(amount, base) {
         var baseHTML = base.html();
         base.css("position", "relative");
         var roof = createChildFace(undefined, undefined, base);
         roof.html(baseHTML);
+        var floor = createChildFace(undefined, undefined, base);
         var wallN = createChildFace(undefined, amount, base);
         var wallS = createChildFace(undefined, amount, base);
         var wallE = createChildFace(undefined, amount, base);
         var wallW = createChildFace(undefined, amount, base);
+        addTransform(floor, "rotateX(-180deg)");
         addTransform(roof, "translateZ(" + amount + "px)");
         addTransform(wallN, "translateY(-50%) rotateX(-90deg) translateY(-50%) rotateY(180deg)");
         addTransform(wallS, "rotateZ(180deg) translateY(-50%) rotateX(-90deg) translateY(-50%) rotateY(180deg)");
         addTransform(wallE, "rotateZ(90deg) translateY(-50%) rotateX(-90deg) translateY(-50%) rotateY(180deg)");
         addTransform(wallW, "rotateZ(270deg) translateY(-50%) rotateX(-90deg) translateY(-50%) rotateY(180deg)");
-        return { roof: roof, walls: [wallN, wallE, wallS, wallW] };
+        [roof, floor, wallN, wallE, wallS, wallW].forEach(function (x) { return createBackFace(x.addClass(classFaceOuter).addClass(classFaceOuterFront)); });
+        return { floor: floor, roof: roof, walls: [wallN, wallE, wallS, wallW] };
     }
     function createCube(size) {
         var base = createChildFace(size, size);
-        var _a = pullOut(size, base), roof = _a.roof, walls = _a.walls;
-        return { base: base, roof: roof, walls: walls };
+        var _a = pullOut(size, base), floor = _a.floor, roof = _a.roof, walls = _a.walls;
+        return { base: base, floor: floor, roof: roof, walls: walls };
     }
     $(function () {
         // var plane = $("#plane");
@@ -53,6 +69,10 @@ define(["require", "exports", "jquery"], function (require, exports, $) {
         // cube.walls[1].css("background","#ccc");
         // cube.walls[2].css("background","#bbb");
         // cube.walls[3].css("background","#aaa");
-        pullOut(100, $("#pot"));
+        var root = $("#pot").css("position", "relative");
+        createCube(100).base.appendTo(root);
+        for (var i = 0; i < 100; ++i) {
+            createCube(100).base.css("position", "absolute").css("transform", "translate3D(" + (Math.random() - 0.5) * 1000 + "px, " + (Math.random() - 0.5) * 1000 + "px, " + (Math.random() - 0.5) * 1000 + "px)").appendTo(root);
+        }
     });
 });
--- main.ts
+++ main.ts
@@ -6,10 +6,11 @@ function setPerspective(container: JQuery, perspective: number): void
 {
 	container.css("perspective", `${perspective}px`);
 }
-function addTransform(container: JQuery, transform: string): void
+function addTransform(container: JQuery, transform: string): JQuery
 {
 	var old = container.css("transform") || "none";
 	container.css("transform", transform + (old == "none" ? "" : " " + old));
+    return container;
 }
 
 
@@ -31,29 +32,50 @@ function createChildFace(width?: number, height?: number, container?: JQuery): J
 	return face;
 }
 
-function pullOut(amount: number, base: JQuery): { roof: JQuery, walls: [JQuery, JQuery, JQuery, JQuery] }
+var classFaceOuter = "faceOuter";
+var classFaceOuterFront = "faceOuterFront";
+var classFaceOuterBack = "faceOuterBack";
+var classFaceBlock = "faceBlock";
+
+function createBackFace(front: JQuery): JQuery
+{
+    return front;
+	var back = createChildFace(undefined, undefined, front);
+    back.addClass(classFaceOuter).addClass(classFaceOuterBack);
+    
+    // create view-block
+	var block = createChildFace(undefined, undefined, front);
+    block.addClass(classFaceBlock);
+    
+    return back;
+}
+
+function pullOut(amount: number, base: JQuery): { floor: JQuery, roof: JQuery, walls: [JQuery, JQuery, JQuery, JQuery] }
 {
 	var baseHTML = base.html();
 	base.css("position", "relative");
 	var roof = createChildFace(undefined, undefined, base);
 	roof.html(baseHTML);
+	var floor = createChildFace(undefined, undefined, base);
 	var wallN = createChildFace(undefined, amount, base);
 	var wallS = createChildFace(undefined, amount, base);
 	var wallE = createChildFace(undefined, amount, base);
 	var wallW = createChildFace(undefined, amount, base);
+	addTransform(floor, `rotateX(-180deg)`);
 	addTransform(roof, `translateZ(${amount}px)`);
 	addTransform(wallN, "translateY(-50%) rotateX(-90deg) translateY(-50%) rotateY(180deg)");
 	addTransform(wallS, "rotateZ(180deg) translateY(-50%) rotateX(-90deg) translateY(-50%) rotateY(180deg)");
 	addTransform(wallE, "rotateZ(90deg) translateY(-50%) rotateX(-90deg) translateY(-50%) rotateY(180deg)");
 	addTransform(wallW, "rotateZ(270deg) translateY(-50%) rotateX(-90deg) translateY(-50%) rotateY(180deg)");
-	return { roof: roof, walls: [wallN, wallE, wallS, wallW] };
+	[roof, floor, wallN, wallE, wallS, wallW].forEach(x => createBackFace(x.addClass(classFaceOuter).addClass(classFaceOuterFront)));
+    return { floor: floor, roof: roof, walls: [wallN, wallE, wallS, wallW] };
 }
 
-function createCube(size: number): { base: JQuery, roof: JQuery, walls: [JQuery, JQuery, JQuery, JQuery] }
+function createCube(size: number): { base: JQuery, floor: JQuery, roof: JQuery, walls: [JQuery, JQuery, JQuery, JQuery] }
 {
 	var base = createChildFace(size, size);
-	var { roof, walls } = pullOut(size, base);
-	return { base: base, roof: roof, walls: walls };
+	var { floor, roof, walls } = pullOut(size, base);
+	return { base: base, floor: floor, roof: roof, walls: walls };
 }
 
 
@@ -67,5 +89,15 @@ $(() => {
 	// cube.walls[2].css("background","#bbb");
 	// cube.walls[3].css("background","#aaa");
 	
-	pullOut(100, $("#pot"));
+    var root = $("#pot").css("position", "relative");
+	//pullOut(10, root);
+    
+    // HAPPY
+    //addTransform(createChildFace(300, 300, root).css("background", "red"), "translate3D(0, 0, 50px)");
+    //addTransform(createChildFace(100, 100, root).css("background", "red"), "translate3D(200px, 0, 50px)");
+    createCube(100).base.appendTo(root);
+    for (var i = 0; i < 100; ++i)
+    {
+        createCube(100).base.css("position", "absolute").css("transform", `translate3D(${(Math.random() - 0.5) * 1000}px, ${(Math.random() - 0.5) * 1000}px, ${(Math.random() - 0.5) * 1000}px)`).appendTo(root);
+    }
 });
